1a4e003fc05ad3393103593d5e8217947a7e65dc
thrownOut <55532782+thrownOut@users.noreply.github.com>
2021-11-28 19:31:46 +0530

    Added support for file selection
    used tkinter to select required pcap file

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import sqlite3
 import scapy.all as scapy
+from tkinter.filedialog import askopenfilename
 
 class Vendor:
-    def __init__(self, vendor_tuple):
-        self.Registry, self.Assignment, self.Organization, self.Address = vendor_tuple
+    def __init__(self, vendor_tuple, mac_addr):
+        self.Registry, self.Assignment, self.Organization, self.Address = vendor_tuple 
+        self.mac_addr = mac_addr
 
     def __str__(self):
-        return f"Registry - {self.Registry}, Assignment - {self.Assignment}, Organization - {self.Organization}, Address - {self.Address}"
+        return f"Mac Address - {self.mac_addr}, Registry - {self.Registry}, Assignment - {self.Assignment}, Organization - {self.Organization}, Address - {self.Address}"
+    
+    def __eq__(self, other):
+        return self.Assignment == other.Assignment
+    
+    def __hash__(self):
+        return int(self.Assignment, 16)
 
 def get_vendors(mac_addr):
     '''Input - Mac Address
@@ -14,10 +22,10 @@ def get_vendors(mac_addr):
     '''
     con = sqlite3.connect("mac.db")
     cur = con.cursor()
-    result = cur.execute("SELECT * FROM mac WHERE Assignment = ?", [mac_addr]).fetchall()
+    result = cur.execute("SELECT * FROM mac WHERE Assignment = ?", [format_mac(mac_addr)]).fetchall()
     vendors = []
     for i in result:
-        vendors.append(Vendor(i))
+        vendors.append(Vendor(i, mac_addr))
     con.close()
     return vendors
 
@@ -27,10 +35,27 @@ def format_mac(mac_addr):
     fmac_addr = fmac_addr.upper()
     return fmac_addr
 
-file = input("Enter file(pcap) path : ")
+print("Enter file(pcap) path : ")
+file = askopenfilename()
+print(file)
 pckts = scapy.rdpcap(file)
+vendor_set = set()
+fake_addrs = set()
 for pckt in pckts:
-    mac_addr = pckt['Ethernet'].src
-    vendors = get_vendors(format_mac(mac_addr))
-    for vendor in vendors:
-        print(vendor)
+    try :
+        mac_addr = pckt['Ethernet'].src
+        vendors = get_vendors(mac_addr)
+        for vendor in vendors:
+            vendor_set.add(vendor)
+        if not vendors:
+            fake_addrs.add(mac_addr)
+    except:
+        continue
+
+print("Real Mac Addresses with Vendors : ")
+for vendor in vendor_set:
+    print(vendor)
+
+print("\nFake Mac Addresses : ")
+for mac_addr in fake_addrs:
+    print(mac_addr)
